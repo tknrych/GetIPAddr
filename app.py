7d022439396d011cfd310dcaad59d3754c19d047
Ryuichi <ryuichi.takano@jp.ricoh.com>
2024-04-26 13:47:18 +0900

    First Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   if request.headers.getlist("X-Forwarded-For"):
+      ipaddr = request.headers.getlist("X-Forwarded-For")[0]
+   else:
+      ipaddr = request.remote_addr
+   return ipaddr
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+   app.run()
