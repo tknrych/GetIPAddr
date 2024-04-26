4b37a759a87482241c6e6490b2ef410bc0180cef
Ryuichi Takano <45957617+tknrych@users.noreply.github.com>
2024-04-27 00:25:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                  send_from_directory, url_for)
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
    print('Request for index page received')
-   if request.headers.getlist("X-Forwarded-For"):
-      ipaddr = request.headers.getlist("X-Forwarded-For")[0]
-   else:
-      ipaddr = request.remote_addr
-   return ipaddr
+   ipaddr = request.remote_addr
+   return request.remote_addr, 200
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+   return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
