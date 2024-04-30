b198d1554a7a2807b7bca1d47af7ba1ceb516513
Ryuichi Takano <45957617+tknrych@users.noreply.github.com>
2024-04-30 23:47:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,40 @@ import os
 
 from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
 from flask import jsonify
+import socket
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def index():
-   print('Request for index page received')
-   if request.headers.getlist("X-Forwarded-For"):
-      print('X-Forwarded-For')
-      ipaddr = request.headers.getlist("X-Forwarded-For")[0]
-   else:
-      print('remote_addr')
-      ipaddr = request.remote_addr
-   
-   return ipaddr, 200
+    print('Request for index page received')
+    if request.headers.getlist("X-Forwarded-For"):
+        print('X-Forwarded-For')
+        ipaddrlst = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        print('remote_addr')
+        ipaddrlst = request.remote_addr
+
+    hostnamelst = []
+    for ipaddr in ipaddrlst.split(","):
+        try:
+            hostnamelst.append(socket.gethostbyaddr(ipaddr)[0])
+        except:
+            hostnamelst.append('n/a')
+
+    ipinfo = {'ip':ipaddrlst, 'hostname':','.join(hostnamelst)}
+
+    return jsonify(ipinfo), 200
 
 @app.route('/favicon.ico')
 def favicon():
-   return send_from_directory(os.path.join(app.root_path, 'static'),
-          'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+        'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route("/ping", methods=["GET"])
 def ping():
     status_code = 200
     return "Ping OK \(^-^)/\n", status_code
-
+   
 if __name__ == '__main__':
    app.run()
